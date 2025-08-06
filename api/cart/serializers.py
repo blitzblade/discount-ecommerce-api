d40c81fc4e895b7356ab9b8a12239900d9c9c7b8
kryzbone <kryzbone@gmail.com>
2025-08-06 23:06:41 +0000

    fix cart item

--- api/cart/serializers.py
+++ api/cart/serializers.py
@@ -6,12 +6,6 @@ from .models import Cart, CartItem
 
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductReadSerializer(read_only=True)
-    product_id = serializers.PrimaryKeyRelatedField(
-        queryset=CartItem._meta.get_field("product").related_model.objects.all(),
-        source="product",
-        write_only=True,
-    )
 
     class Meta:
         model = CartItem
@@ -19,13 +13,12 @@ class CartItemSerializer(serializers.ModelSerializer):
             "id",
             "cart",
             "product",
-            "product_id",
             "quantity",
             "price",
             "created_at",
             "updated_at",
         ]
-        read_only_fields = ["id", "cart", "product", "price", "created_at", "updated_at"]
+        read_only_fields = ["id", "cart", "price", "created_at", "updated_at"]
 
 
 class CartSerializer(serializers.ModelSerializer):
